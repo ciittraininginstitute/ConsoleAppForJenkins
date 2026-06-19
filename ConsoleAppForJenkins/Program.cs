b0f6cc5946421c094db7721134a243de9de5c956
ciittraininginstitute <ciitinstitutepune@gmail.com>
2026-06-19 13:59:30 +0530

    Add project files.

---
+++ ConsoleAppForJenkins/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppForJenkins
+{
+    internal class Program
+    {
+        public void Add()
+        {
+            int a, b, c;
+            a = 10;
+            b = 20;
+            c = a + b;
+            Console.WriteLine("Add=" + c);
+        }
+        static void Main(string[] args)
+        {
+            Program p=new Program();
+            p.Add();
+        }
+    }
+}
